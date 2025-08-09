7fc33ee1ba0a0c81b427f6744c85014423b7cc87
Allih02 <138991986+Allih02@users.noreply.github.com>
2025-08-09 14:13:27 +0300

    Delete cloth directory

--- cloth/auth/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../includes/config.php';
-require_once '../includes/auth.php';
-
-// Destroy session and redirect to login
-session_destroy();
-header("Location: login.php?logout=1");
-exit();
-?>
